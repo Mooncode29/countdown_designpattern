6e6f9febd1b9a7c565a173263994018fdce37cde
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-18 12:00:57 +0200

    count down avec la methode this ok

--- obect.js
+++ obect.js
@@ -1,40 +1,44 @@
+"use strict"
 console.log('hello.world');
+console.log(this.document === document);
+
 (function(){
 	var app = {
 		defaultTimer :5,
 		timer : null,
 		intervalID : null,
 		init : function(){
-			app.timer = app.defaultTimer;
-			app.listners();
+			this.timer = this.defaultTimer;
+			this.listners();
 		},
 		listners : function(){
-			$("#start").on('click', app.start);
-			$("#reset").on('click', app.reset);
-			$("#stop").on('click', app.stop);
+			$("#start").on('click', this.start.bind(this));
+			$("#reset").on('click', this.reset.bind(this));
+			$("#stop").on('click', this.stop.bind(this));
 
 		},
 		start : function(){
-			clearInterval(app.intervalID);
-			app.intervalID = setInterval(app.decrementation, 1000);
+			clearInterval(this.intervalID);
+			this.intervalID = setInterval(this.decrementation.bind(this), 1000);
 		},
 
 		decrementation : function(){
-			app.updateView();
-			app.timer--;
-			if(app.timer <= 0){
-				app.timer = 0;
+			this.updateView();
+			this.timer--;
+			if(this.timer <= 0){
+				this.timer = 0;
 			}
 		},
 
 		stop: function(){
-			clearInterval(app.intervalID);
+			clearInterval(this.intervalID);
 
 		},
 
 		updateView : function(){
-			var minutes = parseInt(app.timer/60, 10);
-			var secondes = parseInt(app.timer % 60, 10);
+			
+			var minutes = parseInt(this.timer/60, 10);
+			var secondes = parseInt(this.timer % 60, 10);
 			if(minutes < 10){
 				minutes = '0'+ minutes; 
 			}
@@ -46,8 +50,8 @@ console.log('hello.world');
 		},
 
 		reset : function(){
-			app.timer = app.defaultTimer;
-			app.updateView();
+			this.timer = this.defaultTimer;
+			this.updateView();
 		},
 	};
 	app.init();
